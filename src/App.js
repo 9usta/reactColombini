2ca114c64a73512e56a3aa454a5b77be17d521ad
Ramiro <ramifazio@gmail.com>
2022-11-02 19:19:08 -0300

    Sugerencias!

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
-import './App.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import NavBar from "./components/NavBar";
+import ItemListContainer from "./components/ItemListContainer";
 
-import ItemListContainer from './components/ItemListContainer';
-
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const darkTheme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
   },
 });
 
 function App() {
-
   return (
-
     <div>
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
       </ThemeProvider>
-      <Navbar />
-      <ItemListContainer greeting={"mensaje dentro del contenedor"} />
+      <NavBar />
+      {
+        //! Estaba mal escrito!! 
+      }
+      <ItemListContainer greeting="mensaje dentro del contenedor" />
     </div>
-
   );
 }
 
 export default App;
-
